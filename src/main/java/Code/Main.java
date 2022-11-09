d8243cd4e83339e0ccfc9236592e54f8285decf7
Alumno_FP <jorgeotin@gmail.com>
2022-11-09 14:09:35 +0100

    09/11/2022

--- src/main/java/Code/Main.java
+++ src/main/java/Code/Main.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import Clases.Proyecto;
+import Clases.listaProyectos;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -17,45 +19,52 @@ import jakarta.xml.bind.Unmarshaller;
 
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		//Creamos un archivo que apunta a insti.xml
-		File xmlCentros = new File("src/main/resources/centros.xml");
-		Path archivo = Path.of("target/centros.json");
-		try
-		{
-			//Creamos el contexto para trabajar con nuestra clase Instituto.
-			JAXBContext contexto = JAXBContext.newInstance(Centros.class);
+        //Creamos un archivo que apunta a insti.xml
+        File xmlCentros = new File("src/main/resources/centros.xml");
+        File xmlCentros1 = new File("src/main/resources/proyectos.xml");
+        Path archivo = Path.of("target/centros.json");
+        Path archivo1 = Path.of("target/Proyecto.json");
+        try {
+            //Creamos el contexto para trabajar con nuestra clase Instituto.
+            JAXBContext contexto = JAXBContext.newInstance(Centros.class);
+            JAXBContext contexto1 = JAXBContext.newInstance(Proyecto.class);
 
-			//Con el objeto tipo Unmarshaller pasamos de XML a Java.
-			Unmarshaller objetoUnmarshaller = contexto.createUnmarshaller();
-			Centros centro;	//Creamos un objeto tipo Centros.
+            //Con el objeto tipo Unmarshaller pasamos de XML a Java.
+            Unmarshaller objetoUnmarshaller = contexto.createUnmarshaller();
+            Unmarshaller objetoUnmarshaller1 = contexto1.createUnmarshaller();
+            Centros centro;    //Creamos un objeto tipo Centros.
+            listaProyectos listaProyectos;
 
-			//Pasamos de XML a Java y mostramos por pantalla el contenido de la etiqueta <nombre>.
-			centro = (Centros) objetoUnmarshaller.unmarshal(xmlCentros);
-		
-			System.out.println(centro.getNombre());
-			System.out.println("\n" + "\n");
-			
-			//Pasamos de objeto a archivo .json
-			
-			GsonBuilder builder = new GsonBuilder();
+            //Pasamos de XML a Java y mostramos por pantalla el contenido de la etiqueta <nombre>.
+            centro = (Centros) objetoUnmarshaller.unmarshal(xmlCentros);
+            listaProyectos = (Clases.listaProyectos) objetoUnmarshaller1.unmarshal(new File("src/main/resources/proyectos.xml"));
 
-			Gson gson = builder.setPrettyPrinting().create();
 
-			String texto = gson.toJson(centro);
-			
-			Files.writeString(archivo, texto);
+            System.out.println(centro.getNombre());
+            System.out.println("\n" + "\n");
 
-		} catch (JAXBException e)
-		{
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+            //Pasamos de objeto a archivo .json
 
-	}
+            GsonBuilder builder = new GsonBuilder();
+
+            Gson gson = builder.setPrettyPrinting().create();
+
+            String texto = gson.toJson(centro);
+            String texto1 = gson.toJson(listaProyectos);
+
+            Files.writeString(archivo, texto);
+            Files.writeString(archivo1, texto1);
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+    }
 
 }
