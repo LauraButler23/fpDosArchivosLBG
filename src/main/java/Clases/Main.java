f3ddbfabb2f3beeda25abffb6d1ee2ff2c5b370c
LauraButler23 <190062@iessanalberto.com>
2022-12-13 18:18:01 +0100

    Añado la clase Usuario y el archivo usuarios.xml Creo el archivo Usuarios.json Proyecto finalizado.

--- src/main/java/Clases/Main.java
+++ src/main/java/Clases/Main.java
@@ -13,26 +13,39 @@ import java.nio.file.Path;
 public class Main {
     public static void main(String[] args) throws JAXBException {
 
-        JAXBContext jaxbContext = JAXBContext.newInstance(listaProyectos.class, Centros.class);
+        //Creamos el jaxb para hacer referencia a las anotaciones de cada clase que le pasamos por parámetro.
+        JAXBContext jaxbContext = JAXBContext.newInstance(listaProyectos.class, Centros.class, Usuario.class);
 
+        //Pasamos de xml a objeto con unmarshaller.
         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
 
+        //Creamos un objeto de cada clase.
         listaProyectos listaProyectos;
         Centros listacentros;
+        Usuario listaUsuarios;
+
+        //Hacemos la instancia de cada objeto desde el xml.
         listaProyectos = (Clases.listaProyectos) unmarshaller.unmarshal(new File("src/main/resources/proyectos.xml"));
         listacentros = (Centros) unmarshaller.unmarshal(new File("src/main/resources/centros.xml"));
+        listaUsuarios = (Usuario) unmarshaller.unmarshal(new File("src/main/resources/usuarios.xml"));
 
+        //Creamos tres archivos .json
         Path path = Path.of("target/Proyecto.json");
         Path path2 = Path.of("target/Centros.json");
+        Path path3 = Path.of("target/Usuarios.json");
 
+        //Creamos un objeto Gson para poder almacenar el texto que queremos almacenar en cada archivo.
         GsonBuilder gsonBuilder = new GsonBuilder();
         Gson gson = gsonBuilder.setPrettyPrinting().create();
         String textoProyecto = gson.toJson(listaProyectos);
         String textoCentros = gson.toJson(listacentros);
+        String textoUsuarios = gson.toJson(listaUsuarios);
 
+        //Escribimos el texto a los archivos.
         try {
            Files.write(path, textoProyecto.getBytes());
            Files.write(path2, textoCentros.getBytes());
+            Files.write(path3, textoUsuarios.getBytes());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
